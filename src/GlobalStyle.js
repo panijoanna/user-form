aaa32d4bde7de9ca1ba8d7670518b5c746f96b52
Joanna Panimasz <pani.joanna93@gmail.com>
2024-05-17 15:21:46 +0200

    feat: install and add styled-components

---
+++ src/GlobalStyle.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+html {
+  box-sizing: border-box;
+}
+
+*,
+::before,
+::after {
+  box-sizing: inherit;
+}
+
+body {
+  margin: 0;
+  font-family: "Montserrat", sans-serif;
+}`;
