c99e6564993710f262a39173b5c93d00151c3dd7
Joanna Panimasz <pani.joanna93@gmail.com>
2024-05-17 15:29:19 +0200

    feat: add routes to the App

--- src/core/App.js
+++ src/core/App.js
@@ -1,3 +1,15 @@
-function App() {}
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/create-user" element={}/>
+        <Route path="/show-user" element={}/>
+        <Route />
+      </Routes>
+    </>
+  );
+}
 
 export default App;
