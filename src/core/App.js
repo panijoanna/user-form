55ef1fafcf5df64bfe9f2a5ec00710e45ca36a90
Joanna Panimasz <pani.joanna93@gmail.com>
2024-05-18 16:33:20 +0200

    feat: add browser router and routes

--- src/core/App.js
+++ src/core/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+import UserForm from "../components/UserForm";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/create-user" element={}/>
-        <Route path="/show-user" element={}/>
+        <Route path="/" element={<UserForm />} />
+        <Route path="/create-user" />
+        <Route path="/show-user" />
         <Route />
       </Routes>
     </>
