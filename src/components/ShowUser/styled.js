8ab4321eb558cd295633cb00ac499729be4c78fa
Joanna Panimasz <pani.joanna93@gmail.com>
2024-05-20 13:56:34 +0200

    feat: add styles and components to the Show User component

--- src/components/ShowUser/styled.js
+++ src/components/ShowUser/styled.js
@@ -1,3 +1,22 @@
 import styled from "styled-components";
 
 export const StyledSection = styled.section``;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: end;
+  background: #1f0d1b;
+  padding: 5px 30px 5px 5px;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  top: 40px;
+  filter: brightness(40%);
+`;
+
+export const TableWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
